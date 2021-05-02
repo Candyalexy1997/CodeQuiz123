427d5a6f6d5afdb9d41c80d31feadb09226b9412
Candice Whiskey <candicewhiskey77@gmail.com>
2021-05-01 21:50:10 -0400

    js variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 //Variables to be created
+var timeInterval = 0;
+var timeRemaining = 60;
+var timeClock = document.querySelector("#timer");
+var yourScore = document.getElementById ("yourScore");
+var questions = document.getElementById("question");
+var i = 0;
+var optionAEL = document.getElementById ("OptionA");
+var optionBEL = document.getElementById ("OptionB");
+var optionCEL = document.getElementById ("OptionC");
+var optionDEL = document.getElementById ("OptionD");
