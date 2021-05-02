190a7d29b2d90c43df26b312677737f1f3562774
Candice Whiskey <candicewhiskey77@gmail.com>
2021-05-01 22:05:34 -0400

    added 1 question to js file

---
+++ assets/js/questions.js
@@ -0,0 +1,10 @@
+//Questions for the Quiz
+
+var questionArr = {
+    question: "What does the "C" in CSS stand for?
+    optionA : "color",
+    optionB : "cascading",
+    optionC : "none",
+    optionD : "cloud",
+    correct : "B"
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,27 @@ var timeRemaining = 60;
 var timeClock = document.querySelector("#timer");
 var yourScore = document.getElementById ("yourScore");
 var questions = document.getElementById("question");
+var hello = document.getElementById("hello");
 var i = 0;
 var optionAEL = document.getElementById ("OptionA");
 var optionBEL = document.getElementById ("OptionB");
 var optionCEL = document.getElementById ("OptionC");
 var optionDEL = document.getElementById ("OptionD");
+var quizSection = document.getElementById("quizSection");
+var topScore = document.getElementById ("topScore");
+var leaders = [];
+var list = document.getElementById("top-score-list");
+var submitButton = document.getElementById ("startBtn");
+var topScore = document.getElementById(topScores);
+
+
+
+//initializing
+
+function init(){
+    topScore.style.display ="none";
+    quizSection.style.display="none";
+
+    var lastUser =
+
+}
